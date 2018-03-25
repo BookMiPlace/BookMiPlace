864f7d1df9a7fa493495035c9da8bc7c756f006e
Alan Donohoe <alandonohoe@hotmail.com>
2018-03-25 17:24:49 +0100

    Add sequelize ORM to app

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
+const { Client } = require('pg');
+const { DATABASE_URL } = process.env;
+const db = require('./models');
 const express = require('express');
-const app = express();
 const bodyParser = require('body-parser');
 const port = process.env.PORT || 3000;
+
+const app = express();
+
+require('dotenv').config();
 
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
@@ -11,7 +17,8 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(require('./controllers'));
 
-app.listen(port, function() {
-  console.log('Listening on port ' + port);
+db.sequelize.sync().then(function() {
+  app.listen(port, function() {
+    console.log('Listening on port ' + port);
+  });
 });
-
