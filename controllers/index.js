bb26d2c1fb28ce515c9ca7b16166e4886399369e
Alan Donohoe <alandonohoe@hotmail.com>
2018-04-01 14:45:01 +0100

    Add trip controller and views

--- controllers/index.js
+++ controllers/index.js
@@ -1,8 +1,28 @@
+const db = require('./../models');
 const express = require('express');
 const router = express.Router();
 
 router.get('/', function(req, res) {
   res.render('index');
+});
+
+// Trips
+router.get('/trips', function(req, res) {
+  db.Trip.findAll()
+  .then((trips) => {
+    res.render('trips/index', {
+      trips: trips
+    });
+  });
+});
+
+router.get('/trips/:id', function(req, res) {
+  db.Trip.findById(req.params.id)
+  .then((trip) => {
+    res.render('trips/show', {
+      trip: trip
+    });
+  });
 });
 
 module.exports = router;
